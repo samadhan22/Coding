cc68b74c911190349c69c745489f4e53955aa904
samadhan22 <samadhansalunkhe22@gmail.com>
2022-05-07 10:42:25 +0530

    7_5_2022

---
+++ Basic 1.c
@@ -0,0 +1,27 @@
+////"Hello World"
+// #include <stdio.h>
+// int main()
+// {
+//   printf("Hello world");
+// }
+
+// // printf
+// #include <stdio.h>
+// int main()
+// {
+//   printf("\t hednksj\n");
+//   printf("jdsncjdsb\n");
+//   printf("sdbjakfjf");
+//   printf("\n c programmnckdsjnckjn");
+// }
+
+// // print an integer
+// #include <stdio.h>
+// int main()
+// {
+//   int a;
+//   printf("Enter the intger number");
+//   scanf("%d",&a);
+//   printf("You have entered number %d",a);
+// }
+
